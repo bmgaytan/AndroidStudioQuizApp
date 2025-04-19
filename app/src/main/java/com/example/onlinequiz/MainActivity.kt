c8c6d523c3601670e1f9b6596a54ddc26d5f8825
bmgaytan <bmgaytan3114@gmail.com>
2025-04-19 19:46:26 -0400

    added quizzes to main menu

--- app/src/main/java/com/example/onlinequiz/MainActivity.kt
+++ app/src/main/java/com/example/onlinequiz/MainActivity.kt
@@ -5,11 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.onlinequiz.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
     lateinit var quizModelList : MutableList<QuizModel>
+    lateinit var adapter: QuizListAdapter
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -34,6 +38,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun setupRecyclerView() {
-        TODO("Not yet implemented")
+        adapter = QuizListAdapter(quizModelList)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.adapter = adapter
     }
 }
--- app/src/main/java/com/example/onlinequiz/QuizListAdapter.kt
+++ app/src/main/java/com/example/onlinequiz/QuizListAdapter.kt
@@ -1,32 +1,34 @@
 package com.example.onlinequiz
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.onlinequiz.databinding.QuizItemRecyclerRowBinding
 
 class QuizListAdapter(private val quizModelList : List<QuizModel>) :
     RecyclerView.Adapter<QuizListAdapter.MyViewHolder>() {
-    override fun onCreateViewHolder(
-        parent: ViewGroup,
-        viewType: Int
-    ): MyViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun onBindViewHolder(
-        holder: MyViewHolder,
-        position: Int
-    ) {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
 
     class MyViewHolder(private val binding: QuizItemRecyclerRowBinding) : RecyclerView.ViewHolder(binding.root){
         fun bind(model: QuizModel){
-            //bind all the view
+            //bind all the views
+            binding.apply {
+                quizTitleText.text = model.title
+            }
         }
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = QuizItemRecyclerRowBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(quizModelList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return quizModelList.size
+    }
+
+
 }
